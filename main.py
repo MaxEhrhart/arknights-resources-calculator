b59f7f244475cf18bdc22179adc80ebc2d31d330
Maximilian Erhard <maxserhard@gmail.com>
2021-10-07 01:23:18 -0300

    Resources

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # encoding: utf-8
 import json
-import pandas as pd
+import csv
+# import pandas as pd
 from dataclasses import dataclass
+
+resources_path = 'files/resource.json'
+operators_path = 'files/operator.json'
+user_operators_path = 'files/user_operators.csv'
 
 
 def read_json(file_path: str) -> dict:
@@ -10,6 +15,10 @@ def read_json(file_path: str) -> dict:
         data = json.load(f)
         f.close()
     return data
+
+
+resources_data = read_json(resources_path)
+operators_data = read_json(operators_path)
 
 
 @dataclass
@@ -24,17 +33,34 @@ class Operator:
     stars: int = 0
 
 
-if __name__ == "__main__":
-    resources_path = 'files/resource.json'
-    operators_path = 'files/operator.json'
+def sum_skill_upgrade_resources(upgrades):
+    resources = dict()
+    for level in upgrades:
+        for iteration, resource in enumerate(level['resources']):
+            key = resource['name']
+            value = resource['quantity']
+            resources[key] = resources.get(key, 0) + value
+    return resources
 
-    resources_data = read_json(resources_path)
-    print(resources_data)
 
-    operators_data = read_json(operators_path)
-    print(operators_data)
+def calc_resource(operator: dict):
+    resources = dict()
+    for op in operators_data:
+        if operator['operator'] in op['name']:
+            resources = {**resources, **sum_skill_upgrade_resources(op['skills']['upgrade'])}
+    print(operator['operator'], resources)
 
-    # df_resources = pd.read_json(resources_path)
-    # df_operators = pd.read_json(operators_path)
-    # print(df_resources)
-    # print(df_operators)
+
+# if __name__ == "__main__":
+# print(resources_data)
+# print(operators_data)
+with open(user_operators_path, mode="r", encoding="utf-8") as f:
+    csv_reader = csv.DictReader(f, delimiter=';')
+    for operator in csv_reader:
+        calc_resource(operator)
+
+
+# df_resources = pd.read_json(resources_path)
+# df_operators = pd.read_json(operators_path)
+# print(df_resources)
+# print(df_operators)
