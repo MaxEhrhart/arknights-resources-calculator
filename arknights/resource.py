b053a83452a50d6f211d418685766ff016ab2111
Maximilian Erhard <maxs.erhard@gmail.com>
2021-11-24 12:14:53 -0300

    Update Operators

--- arknights/resource.py
+++ arknights/resource.py
@@ -17,15 +17,22 @@ def get_resources_data():
 class Resource:
     # General
     name: str = None
-    tier: int = 1
+    tier: int = 0
     drop: bool = True
     recipe: list = field(default_factory=list)  # List[Resource]
 
+    def __post_init__(self):
+        """Load resource data."""
+        path = Path(constants.Paths.RESOURCES_PATH.value)
+        resource_json_path = list(path.rglob(f'*/{self.name}.json'))
+        self.json_data = utils.read_json(file_path=str(resource_json_path[0]), show=False)
+        self.tier = self.json_data['tier']
+        self.drop = self.json_data['droppable']
+        self.recipe = self.json_data['recipe']
+        self.lmd_cost = self.json_data['lmd']
+        print(self.json_data)
+
 
 if __name__ == "__main__":
-    orirock = Resource(
-        name='Orirock',
-        tier=1,
-        drop=True
-    )
-    print(orirock.recipe)
+    orirock = Resource(name='Aketon')
+    print(orirock)
