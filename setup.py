ec8b6e48d0cf42b2e192584ea572fb9a0dd06455
MaxEhrhart <maxserhard@gmail.com>
2022-03-19 22:19:45 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='arknights-resources-calculator',
-   version='1.0',
-   description='A useful module for Arknights spent resources calculations',
-   author='MaxEhrhart',
-   author_email='',
-   packages=['arknights']  # ,  #same as name
-   # install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+    name='arknights-resources-calculator',
+    version='1.0.0',
+    description='A useful module for Arknights spent resources calculation',
+    author='MaxEhrhart',
+    author_email='',
+    packages=['arknights'],  # same as name
+    install_requires=['numpy==1.21.4', 'pandas==1.3.4', 'XlsxWriter==3.0.2', 'pandas-xlsx-tables==0.0.5']  # dependency
 )
